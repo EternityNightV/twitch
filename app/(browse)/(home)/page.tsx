4d3c3314a50860e4ed86d025eecbeef8260881fa
EternityNightV <143103542+EternityNightV@users.noreply.github.com>
2023-12-12 18:23:29 +0300

    1.start clerk + sidebar

---
+++ app/(browse)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { SignOutButton } from '@clerk/nextjs'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <SignOutButton/>
+      Sveta  suchka
+    </main>
+  )
+}
